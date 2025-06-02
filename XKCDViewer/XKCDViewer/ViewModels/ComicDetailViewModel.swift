c60bf0a9ade184b45a179b0ca2abf03d8f4a598f
kia-asar <92662951+kia-asar@users.noreply.github.com>
2025-06-02 14:33:32 -0700

    Introducing ComicDetailViewModel and loading state (#3)
    * feat: Introduced ComicDetailViewModel
    
    * feat: Added isLoading state
    
    * feat: Added a spinner as image placeholder while loading

---
+++ XKCDViewer/XKCDViewer/ViewModels/ComicDetailViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  ComicViewModel.swift
+//  XKCDViewer
+//
+//  Created by Kiarash Asar on 6/2/25.
+//
+
+import Foundation
+
+@MainActor
+final class ComicDetailViewModel: ObservableObject {
+    @Published var comic: Comic?
+    @Published var isLoading = false
+    @Published var errorMessage: String?
+    
+    private let xkcdService: XKCDServicing
+    
+    init(xkcdService: XKCDServicing = XKCDService()) {
+        self.xkcdService = xkcdService
+    }
+    
+    func fetchComic(number: Int) async {
+        isLoading = true
+        errorMessage = nil
+        comic = nil
+        
+        do {
+            let fetchedComic = try await xkcdService.fetchComic(number: number)
+            comic = fetchedComic
+        } catch {
+            errorMessage = error.localizedDescription
+        }
+        
+        isLoading = false
+    }
+}
