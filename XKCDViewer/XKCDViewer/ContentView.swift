eb4723f44101a8407b80ad05f6ac2c4c580db411
Kiarash Asar <kiarash.asar@gmail.com>
2025-06-02 11:09:36 -0700

    init: Xcode project setup

---
+++ XKCDViewer/XKCDViewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XKCDViewer
+//
+//  Created by Kiarash Asar on 6/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XKCDViewer/XKCDViewer/XKCDViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  XKCDViewerApp.swift
+//  XKCDViewer
+//
+//  Created by Kiarash Asar on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct XKCDViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
